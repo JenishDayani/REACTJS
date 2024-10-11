2e82b872e7662fdb335972f6194995eec0e22a58
JenishDayani <dayanijenish.prime@gmail.com>
2024-10-11 12:28:57 +0530

    Remove Media query in React

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Radium, { StyleRoot } from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -88,18 +87,16 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
-        <div className='App'>
-          <h1>Hi, I'm a React First Project</h1>
-          <p className={classes.join(' ')}>This is really Working!</p>
-          <button style={style} onClick={this.togglePersonsHandler}>
-            Switch Name
-          </button>
-          {persons}
-        </div>
-      </StyleRoot>
+      <div className='App'>
+        <h1>Hi, I'm a React First Project</h1>
+        <p className={classes.join(' ')}>This is really Working!</p>
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Switch Name
+        </button>
+        {persons}
+      </div>
     );
   }
 }
 
-export default Radium(App);
+export default App;
--- my-first-app/src/Person/Person.js
+++ my-first-app/src/Person/Person.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import Radium from 'radium';
 import './Person.css';
 const person = (props) => {
-  const style = {
-    width: '25%',
-    '@media (min-width: 500px)': {
-      width: '100%',
-    },
-  };
   return (
-    <div className='Person' style={style}>
+    <div className='Person'>
       <p onClick={props.click}>
         I'm a {props.name} and I am {props.age} year old!
       </p>
@@ -19,4 +12,4 @@ const person = (props) => {
   );
 };
 
-export default Radium(person);
+export default person;
